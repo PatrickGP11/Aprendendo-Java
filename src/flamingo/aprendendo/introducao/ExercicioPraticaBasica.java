aed1bff83bb194d21ee3b96bce8df2374f07fffe
Patrick G Pinheiro <patrickgonsalves06@gmail.com>
2026-08-13 19:18:58 -0300

    Refactor ExercicioPraticaBasica with updated details

--- src/flamingo/aprendendo/introducao/ExercicioPraticaBasica.java
+++ src/flamingo/aprendendo/introducao/ExercicioPraticaBasica.java
@@ -1,15 +1,15 @@
 package flamingo.aprendendo.introducao;
 
-public class ExercicioPraticaBasica {
-    //Eu <nome> morando no <endereço>, e confirmo o <salario> na data <data>
-    static void main(String[] args) {
-        String nome = "Patrick";
-        String endereco = "Consolação";
-        double salario = 1800.10;
-        int dia = 12;
-        int mes = 8;
-        int ano = 2006;
+package flamingo.aprendendo.introducao;
 
-        System.out.println("Eu" + nome, "estou morando no" + endereco, "e confirmo o salario de" + salario, "na data de" + dia, + mes, ano);
+public class ExercicioPraticaBasica {
+    public static void main(String[] args) {
+        String nome = "Patrick";
+        String endereco = "Rua das Flores, 123 - São Paulo";
+        double salario = 3500.50;
+        String data = "13/08/2026";
+
+        System.out.println("Eu " + nome + " moro na " + endereco + ", e confirmo o R$ " + salario + " na data " + data);
     }
 }
+

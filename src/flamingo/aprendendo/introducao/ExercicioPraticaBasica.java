03d824c817c9d4c801f5e8f7b61d1e4644e1017c
Patrick G Pinheiro <patrickgonsalves06@gmail.com>
2026-08-12 21:27:48 -0300

    Add files via upload

---
+++ src/flamingo/aprendendo/introducao/ExercicioPraticaBasica.java
@@ -0,0 +1,15 @@
+package flamingo.aprendendo.introducao;
+
+public class ExercicioPraticaBasica {
+    //Eu <nome> morando no <endereço>, e confirmo o <salario> na data <data>
+    static void main(String[] args) {
+        String nome = "Patrick";
+        String endereco = "Consolação";
+        double salario = 1800.10;
+        int dia = 12;
+        int mes = 8;
+        int ano = 2006;
+
+        System.out.println("Eu" + nome, "estou morando no" + endereco, "e confirmo o salario de" + salario, "na data de" + dia, + mes, ano);
+    }
+}
